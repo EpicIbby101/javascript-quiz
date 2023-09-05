7c6fd50fa2ecdf3f9a0b2002167d1333b19e2ce4
EpicIbby101 <kevin_grittner@outlook.com>
2023-09-05 19:35:57 +0100

    Moved questions to a separate file to save space

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -8,43 +8,6 @@ const submitButton = document.getElementById("submit");
 const feedbackElement = document.getElementById("feedback");
 const endScreenElement = document.getElementById("end-screen");
 
-// Questions array
-const questions = [
-  {
-    question: "What does HTML stand for?",
-    choices: [
-      "Hyper Text Markup Language",
-      "Home Tool Markup Language",
-      "Hyperlinks and Text Markup Language",
-    ],
-    correctAnswer: "Hyper Text Markup Language",
-  },
-  {
-    question: "Which language is used for styling web pages?",
-    choices: ["HTML", "CSS", "JavaScript"],
-    correctAnswer: "CSS",
-  },
-  {
-    question: "Which programming language is often used for web development?",
-    choices: ["Python", "Java", "JavaScript"],
-    correctAnswer: "JavaScript",
-  },
-  {
-    question: "Inside which HTML element do we put the JavaScript?",
-    choices: ["<js>", "<script>", "<scripting>", "<javascript>"],
-    correctAnswer: "<script>",
-  },
-  {
-    question: "Where is the correct place to insert JS code?",
-    choices: [
-      "<head>",
-      "<body>",
-      "Both the <head> and <body> section are correct",
-      "<meta>",
-    ],
-    correctAnswer: "Both the <head> and <body> section are correct",
-  },
-];
 
 // Variables for game
 let currentQuestionIndex = 0;
---
+++ assets/js/questions.js
@@ -0,0 +1,36 @@
+const questions = [
+    {
+      question: "What does HTML stand for?",
+      choices: [
+        "Hyper Text Markup Language",
+        "Home Tool Markup Language",
+        "Hyperlinks and Text Markup Language",
+      ],
+      correctAnswer: "Hyper Text Markup Language",
+    },
+    {
+      question: "Which language is used for styling web pages?",
+      choices: ["HTML", "CSS", "JavaScript"],
+      correctAnswer: "CSS",
+    },
+    {
+      question: "Which programming language is often used for web development?",
+      choices: ["Python", "Java", "JavaScript"],
+      correctAnswer: "JavaScript",
+    },
+    {
+      question: "Inside which HTML element do we put the JavaScript?",
+      choices: ["<js>", "<script>", "<scripting>", "<javascript>"],
+      correctAnswer: "<script>",
+    },
+    {
+      question: "Where is the correct place to insert JS code?",
+      choices: [
+        "<head>",
+        "<body>",
+        "Both the <head> and <body> section are correct",
+        "<meta>",
+      ],
+      correctAnswer: "Both the <head> and <body> section are correct",
+    },
+  ];
