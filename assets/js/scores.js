642330837203799ba4ad1c6343e5fd51d1a83d3a
EpicIbby101 <kevin_grittner@outlook.com>
2023-09-05 17:43:32 +0100

    Initialised scores highscore logic

---
+++ assets/js/scores.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const highscoresList = document.getElementById("highscores");
+
+  // Retrieve high scores from localStorage
+  const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+  // Sort high scores in descending order by score
+  highScores.sort((a, b) => b.score - a.score);
+
+  // Iterate through the high scores and create list items to display them
+  highScores.forEach((userData, index) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = `${index + 1}. ${userData.initials}: ${userData.score}`;
+    highscoresList.appendChild(listItem);
+  });
+
+  // Add event listener to the "Clear Highscores" button
+  const clearButton = document.getElementById("clear");
+  clearButton.addEventListener("click", clearHighscores);
+});
+
+function clearHighscores() {
+  // Clear high scores from localStorage
+  localStorage.removeItem("highScores");
+
+  // Clear the displayed high scores on the page
+  const highscoresList = document.getElementById("highscores");
+  highscoresList.innerHTML = "Highscores cleared!";
+}
