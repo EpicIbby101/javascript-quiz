292382b012ecc7ffbbac92a11cfbec38b4ecbfbd
EpicIbby101 <kevin_grittner@outlook.com>
2023-09-05 17:43:02 +0100

    Finalised logic for game

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,22 +1,3 @@
-// Questions array
-const questions = [
-    {
-      question: "What does HTML stand for?",
-      choices: ["Hyper Text Markup Language", "Home Tool Markup Language", "Hyperlinks and Text Markup Language"],
-      correctAnswer: "Hyper Text Markup Language"
-    },
-    {
-      question: "Which language is used for styling web pages?",
-      choices: ["HTML", "CSS", "JavaScript"],
-      correctAnswer: "CSS"
-    },
-    {
-      question: "Which programming language is often used for web development?",
-      choices: ["Python", "Java", "JavaScript"],
-      correctAnswer: "JavaScript"
-    }
-  ];
-
 const startButton = document.getElementById("start");
 const questionsContainer = document.getElementById("questions");
 const choicesContainer = document.getElementById("choices");
@@ -25,52 +6,144 @@ const finalScoreElement = document.getElementById("final-score");
 const initialsInput = document.getElementById("initials");
 const submitButton = document.getElementById("submit");
 const feedbackElement = document.getElementById("feedback");
+const endScreenElement = document.getElementById("end-screen");
+
+// Questions array
+const questions = [
+  {
+    question: "What does HTML stand for?",
+    choices: [
+      "Hyper Text Markup Language",
+      "Home Tool Markup Language",
+      "Hyperlinks and Text Markup Language",
+    ],
+    correctAnswer: "Hyper Text Markup Language",
+  },
+  {
+    question: "Which language is used for styling web pages?",
+    choices: ["HTML", "CSS", "JavaScript"],
+    correctAnswer: "CSS",
+  },
+  {
+    question: "Which programming language is often used for web development?",
+    choices: ["Python", "Java", "JavaScript"],
+    correctAnswer: "JavaScript",
+  },
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<js>", "<script>", "<scripting>", "<javascript>"],
+    correctAnswer: "<script>",
+  },
+  {
+    question: "Where is the correct place to insert JS code?",
+    choices: [
+      "<head>",
+      "<body>",
+      "Both the <head> and <body> section are correct",
+      "<meta>",
+    ],
+    correctAnswer: "Both the <head> and <body> section are correct",
+  },
+];
 
 let currentQuestionIndex = 0;
 let timer;
 let timeRemaining = 60;
 let score = 0;
 
-
 function startQuiz() {
-    startButton.style.display = "none";
-    questionsContainer.style.display = "block";
+  startButton.style.display = "none";
+  questionsContainer.style.display = "block";
+  displayQuestion();
+  startTimer();
+}
+
+function displayQuestion() {
+  const currentQuestion = questions[currentQuestionIndex];
+  document.getElementById("question-title").textContent =
+    currentQuestion.question;
+  choicesContainer.innerHTML = "";
+
+  currentQuestion.choices.forEach((choice) => {
+    const choiceButton = document.createElement("button");
+    choiceButton.textContent = choice;
+    choiceButton.addEventListener("click", () => checkAnswer(choice));
+    choicesContainer.appendChild(choiceButton);
+  });
+}
+
+function checkAnswer(choice) {
+  const currentQuestion = questions[currentQuestionIndex];
+
+  if (choice === currentQuestion.correctAnswer) {
+    feedbackElement.textContent = "Correct!";
+    score++;
+  } else {
+    feedbackElement.textContent = "Wrong! -10 seconds";
+    timeRemaining -= 10;
+  }
+
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex < questions.length) {
     displayQuestion();
-    startTimer();
+  } else {
+    endQuiz();
   }
+}
 
-  function displayQuestion() {
-    const currentQuestion = questions[currentQuestionIndex];
-    document.getElementById("question-title").textContent = currentQuestion.question;
-    choicesContainer.innerHTML = "";
-  
-    currentQuestion.choices.forEach((choice) => {
-      const choiceButton = document.createElement("button");
-      choiceButton.textContent = choice;
-      choiceButton.addEventListener("click", () => checkAnswer(choice));
-      choicesContainer.appendChild(choiceButton);
-    });
+function startTimer() {
+  timer = setInterval(() => {
+    timeRemaining--;
+    timerElement.textContent = timeRemaining;
+
+    if (timeRemaining <= 0) {
+      endQuiz();
+    }
+  }, 1000);
+}
+
+function endQuiz() {
+  clearInterval(timer);
+  questionsContainer.style.display = "none";
+  finalScoreElement.textContent = score;
+  feedbackElement.textContent = "";
+
+  // Show the end-screen when the quiz ends
+  endScreenElement.style.display = "block";
+}
+
+submitButton.addEventListener("click", saveScore);
+
+function saveScore() {
+  const initials = initialsInput.value.trim();
+
+  if (initials !== "") {
+    // Create a new object to store the user's data
+    const userData = {
+      initials: initials,
+      score: score,
+    };
+
+    // Retrieve existing high scores from localStorage or initialize an empty array
+    const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+    // Add the current user's data to the high scores array
+    highScores.push(userData);
+
+    // Sort the high scores by score in descending order
+    highScores.sort((a, b) => b.score - a.score);
+
+    // Store the updated high scores back in localStorage
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+
+    // Redirect the user to highscores.html after saving the score
+    window.location.href = "highscores.html";
+    alert("Score saved successfully!")
   }
+}
 
-  function checkAnswer(choice) {
-    const currentQuestion = questions[currentQuestionIndex];
-  
-    if (choice === currentQuestion.correctAnswer) {
-      feedbackElement.textContent = "Correct!";
-      score++;
-    } else {
-      feedbackElement.textContent = "Wrong! -10 seconds";
-      timeRemaining -= 10;
-    }
-  
-    currentQuestionIndex++;
-
-    if (currentQuestionIndex < questions.length) {
-        displayQuestion();
-      } else {
-        endQuiz();
-      }
-    }
+startButton.addEventListener("click", startQuiz);
 
 // PSEUDOCODE - GENERAL GAME LOGIC
 // When start button is pressed, timer starts and first question appears
